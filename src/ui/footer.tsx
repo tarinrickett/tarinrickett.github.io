f3e9220dbd68f54f645de0b68cd29a5faa0ac6c6
Tarin <tarin@bluedot.org>
2025-03-10 08:34:01 +0000

    Create new simple Next.js React app

---
+++ src/ui/footer.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export function Footer() {
+  return (
+    <footer className="footer flex-grow-1 bg-accent mt-8 w-full h-full min-h-12">
+        {/* TBD */}
+    </footer>
+  );
+}
