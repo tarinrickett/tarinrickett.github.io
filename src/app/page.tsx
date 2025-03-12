2a573e5314cefff31ae1cf07b66c993e99b711bb
Tarin <tarin@bluedot.org>
2025-03-12 07:42:09 +0000

    Another commit to trigger a build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Nav } from "@/ui/nav";
 import { Footer } from "@/ui/footer";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="body flex flex-col bg-canvas min-h-screen">
@@ -7,10 +9,10 @@ export default function Home() {
       <main className="main px-4 sm:px-8 max-w-7xl mx-auto">
         <div className="hero flex flex-col sm:flex-row gap-4 justify-center items-center">
           <div className="hero-image flex-shrink-0">
-            <img src="/tarin--green.png" alt="Tarin" className="w-60 h-60" />
+            <Image src="/tarin--green.png" alt="Tarin" width={240} height={240} />
           </div>
           <div className="hero-body my-auto">
-            <h1 className="hero-title text-4xl font-bold inline">hi, i'm tarin!</h1>
+            <h1 className="hero-title text-4xl font-bold inline">hi, i’m tarin!</h1>
             <p className="hero-text ml-2 inline">
               i am a cross-functional product engineer and technical leader with a deep understanding of people problems, from team dynamics to empathy for our users. i lead workshops and events across diverse communities to demystify technology and empower my audiences. i have a passion for social impact, particularly women in tech and how tech can— and must — be a lever for social good.
             </p>
--- src/ui/nav.tsx
+++ src/ui/nav.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import Image from "next/image";
+
 export function Nav() {
   return (
     <nav className="nav bg-color-canvas w-full max-w-7xl mx-auto px-4 sm:px-8 pt-4 mb-8">
       <div className="nav-logo flex items-center justify-between h-14">
         <a href="#">
-          <img src="/logo--green.png" alt="Tarin" className="w-14 h-14" />
+          <Image src="/logo--green.png" alt="Tarin" width={56} height={56} />
         </a>
 
         <div className="nav-links flex gap-6 ml-4 pb-4 border-b-4 border-[var(--color-accent)] w-full justify-end">
